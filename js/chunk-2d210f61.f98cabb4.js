0c656e744898788ee7febb8ff9ee5e6a0076f29c
Mr Viet <vietdat657@gmail.com>
2020-10-28 09:23:57 +0700

    Resume

---
+++ js/chunk-2d210f61.f98cabb4.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d210f61"],{b9b6:function(e,n,t){"use strict";t.r(n);var o=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[e._v("Kogi-Store")])},r=[],u={name:"Kogi-Store"},c=u,i=t("2877"),l=Object(i["a"])(c,o,r,!1,null,null,null);n["default"]=l.exports}}]);
+//# sourceMappingURL=chunk-2d210f61.f98cabb4.js.map
